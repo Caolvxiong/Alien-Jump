5ef61d8875ee1f49f942cfec66421717efa13b66
Wenchao <wangwenchao1990@gmail.com>
2016-04-08 11:47:24 -0400

    Fix the ad not show bug when come back.

--- app/src/main/java/com/wangwenchao/godown/GameMainActivity.java
+++ app/src/main/java/com/wangwenchao/godown/GameMainActivity.java
@@ -121,6 +121,14 @@ public class GameMainActivity extends Activity {
         super.onResume();
         Assets.onResume();
         sGame.onResume();
+        showBanner();
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     @Override
